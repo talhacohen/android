8f800ba455c8df80b8ca6711ba71695bdc879bdb
Tal Hacohen <hacohen.tal@gmail.com>
2019-02-16 11:14:13 +0200

    Add notificatio when uploading

--- app/src/main/java/com/etesync/syncadapter/ui/secureshare/UploadDialogFragment.kt
+++ app/src/main/java/com/etesync/syncadapter/ui/secureshare/UploadDialogFragment.kt
@@ -1,10 +1,16 @@
 package com.etesync.syncadapter.ui.secureshare
 
 import android.app.Dialog
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.ProgressDialog
+import android.content.Context
 import android.os.AsyncTask
+import android.os.Build
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
+import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
 import com.etesync.syncadapter.R
 import com.etesync.syncadapter.model.SecureShare
 
@@ -21,7 +27,7 @@ class UploadDialogFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val progress = ProgressDialog(context)
-        progress.setTitle(getString(R.string.uploading_files))
+        progress.setTitle(getString(R.string.uploading_file))
         progress.setMessage(getString(R.string.please_wait))
         progress.isIndeterminate = true
         progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL)
@@ -31,6 +37,33 @@ class UploadDialogFragment : DialogFragment() {
     }
 
     private inner class UploadFile : AsyncTask<Void, Int, Void>() {
+        private var notificationManager : NotificationManagerCompat = NotificationManagerCompat.from(context!!)
+
+        private fun createNotificationChannel() {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val name = getString(R.string.notification_channel_name)
+                val descriptionText = getString(R.string.notification_channel_description)
+                val importance = NotificationManager.IMPORTANCE_DEFAULT
+                val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                    description = descriptionText
+                }
+                // Register the channel with the system
+                val notificationManager: NotificationManager =
+                        context!!.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.createNotificationChannel(channel)
+            }
+            }
+
+        override fun onPreExecute() {
+            createNotificationChannel()
+            val builder = NotificationCompat.Builder(context!!, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_sync_dark)
+                    .setContentTitle(context!!.getString(R.string.app_name))
+                    .setContentText(context!!.getString(R.string.uploading_file))
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT).build()
+
+            notificationManager.notify(secureShare.fileName.hashCode(), builder)
+        }
 
         override fun doInBackground(vararg voids: Void): Void? {
             // TODO(talh): need to call UploadFile only after the dialog is created
@@ -44,6 +77,7 @@ class UploadDialogFragment : DialogFragment() {
         }
 
         override fun onPostExecute(result: Void) {
+            notificationManager.cancel(secureShare.fileName.hashCode())
             dismiss()
             if (activity is UploadFileCallbacks) {
                 (activity as UploadFileCallbacks).onUpload(secureShare, "FILE_URL")
@@ -60,6 +94,8 @@ class UploadDialogFragment : DialogFragment() {
     }
 
     companion object {
+        private val CHANNEL_ID = "EteSync_SecureShare"
+
         private val KEY_TIME_LIMIT = "timeLimit"
 
         fun newInstance(secureShare: SecureShare, timeLimit: Int): UploadDialogFragment {
